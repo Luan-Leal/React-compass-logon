777260c6fae6911fadeb25347ef8878c071f84f9
Luan Leal <luan.leal@compasso.com.br>
2022-07-05 16:33:05 -0300

    feat: loginpage finished

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -4,28 +4,36 @@ import{useNavigate} from "react-router-dom"
 import "./index.css";
 import Input from "../../components/Form/index";
 import Button from "../../components/Button/index";
+
 import WhiteLogo from "../../assets/white-logo.png";
+import UserIcon from "../../assets/user-icon.png"
+import PasswordIcon from "../../assets/password-icon.png"
 
 function Login() {
   const nav = useNavigate()
   const [user, setUser] = useState("")
   const [password, setPassword] = useState("")
+  const [error, setError] = useState(null)
 
   const handleSaveUser = (event) => {
     event.preventDefault()
     const data = {user, password}
     if(data.user && data.password === "admin"){
-      alert("certo")
       nav("/home")
+      setError(false)
     }else{
-      alert("errado")
+      setError(true)
     }
   }
 
   return (
     <section className="login-page">
       <div className="left">
+        <div>
+          <img className="white-logo" src={WhiteLogo} alt="Compass logo" />
+        </div>
         <div className="welcome-phase">
+
           <h1>Olá,</h1>
           <p>
             Para continuar navegando de forma segura, efetue o login na rede.
@@ -34,29 +42,34 @@ function Login() {
         <form>
           <label>
             <h2 className="login-text">Login</h2>
-
-            <Input type="name" placeholder="Usuário" name="user"
-            value={user}
-            onChange={event => setUser(event.target.value)}
-            />
-
-            <Input type="password" placeholder="Senha" name="password"
-            value={password}
-            onChange={event => setPassword(event.target.value)}
-            ></Input>
-
-            {
-            <div className="error">
-              <span>Ops, usuário ou senha inválidos. Tente novamente!</span>
+            <div className="user">
+              <Input type="name" placeholder="Usuário" name="user"
+              value={user}
+              onChange={event => setUser(event.target.value)}
+              />
+              <i><img src={UserIcon} alt="" /> </i>
             </div>
-            }
+
+            <div className="password">
+              <Input type="password" placeholder="Senha" name="password"
+              value={password}
+              onChange={event => setPassword(event.target.value)}
+              ></Input>
+              <i><img src={PasswordIcon} alt="" /> </i>
+            </div>
+            
+
+            <div className="error">
+              { error &&<span>Ops, usuário ou senha inválidos. Tente novamente!</span>}
+            </div>
+
            <Button text="Continuar" onClick={handleSaveUser}/>
           </label>
         </form>
       </div>
 
       <div className="right">
-        <img className="white-logo" src={WhiteLogo} alt="" />
+        <img className="white-logo" src={WhiteLogo} alt="Compass logo" />
       </div>
     </section>
   );
