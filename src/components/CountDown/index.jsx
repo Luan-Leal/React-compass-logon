7b949d4fc8d484403bc8726b7e771c14497999f4
Luan Leal <luan.leal@compasso.com.br>
2022-07-06 15:08:23 -0300

    feat: timer finished

--- src/components/CountDown/index.jsx
+++ src/components/CountDown/index.jsx
@@ -3,9 +3,21 @@ import { useNavigate } from "react-router-dom";
 import "./index.css";
 
 function CountDown() {
+  const nav = useNavigate();
+  const [counter, setCounter] = React.useState(600);
+
+  React.useEffect(() => {
+    const timer =
+      counter > 0 && setInterval(() => setCounter(counter - 1), 1000);
+    return () => clearInterval(timer);
+  }, [counter]);
+  if (counter === 0) {
+    nav("/");
+  }
+
   return (
     <div>
-      <span className="timer">600</span>
+      <span className="timer">{counter}</span>
       <p className="seconds">seconds</p>
     </div>
   );
