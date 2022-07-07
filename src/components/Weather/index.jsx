de876733bca71f8bf415fa6a4f41313b4519534b
Luan Leal <luan.leal@compasso.com.br>
2022-07-07 17:17:53 -0300

    feat/ weather api working

--- src/components/Weather/index.jsx
+++ src/components/Weather/index.jsx
@@ -1,12 +1,66 @@
-import "./index.css"
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+
+import WeatherIcon from "../../assets/icons/01d.png";
+import UnknownIcon from "../../assets/icons/unknown.png";
+
+import "./index.css";
 
 function Weather() {
+  const [location, setLocation] = useState(false);
+  const [weather, setWeather] = useState(false);
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      getWeather(position.coords.latitude, position.coords.longitude);
+      setLocation(true);
+    });
+  }, []);
+
+  let getWeather = async (lat, long) => {
+    let res = await axios.get(
+      "http://api.openweathermap.org/data/2.5/weather",
+      {
+        params: {
+          lat: lat,
+          lon: long,
+          appid: "64ed82577ced7f69cb1687f0ce536131",
+          lang: "pt",
+          units: "metric",
+        },
+      }
+    );
+    setWeather(res.data);
+    console.log(res.data);
+  };
+
+  if (location == false) {
     return (
-        <div className="weather">
-            <span>Local</span>
-            <span>Clima</span>
+      <div className="weather-warning">
+        <span>
+          <img src={UnknownIcon} alt="Localização desligada" />
+        </span>
+      </div>
+    );
+  } else if (weather == false) {
+    return (
+      <div>
+        <span>Carregando o clima...</span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="weather">
+        <span className="place">
+          {weather.name} - {weather.sys.country}
+        </span>
+        <div className="temp">
+          <img src={WeatherIcon} alt="" />
+          <span>{weather.main.temp.toFixed(0)}°</span>
         </div>
-    )
+      </div>
+    );
+  }
 }
 
-export default Weather
+export default Weather;
